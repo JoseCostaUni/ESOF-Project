fa96d42bcb1c00a2f589cbf3d3b9753bb6d23063
Bruno Miguel Ataide Fortes <52974677+Bruno081002@users.noreply.github.com>
2024-03-22 18:20:50 +0000

    Updating Register page

--- vertical_prototype/Vertical Prototype/lib/main.dart
+++ vertical_prototype/Vertical Prototype/lib/main.dart
@@ -37,14 +37,22 @@ class _LoginDemoSate extends State<LoginDemo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const SizedBox(height: 60.0, width: 20.0,),
+            const SizedBox(
+              height: 60.0,
+              width: 20.0,
+            ),
             Padding(
               padding: const EdgeInsets.only(left: 30.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
                   GestureDetector(
-                    onTap: () {},
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) => const LoginDemo()),
+                      );
+                    },
                     child: const Text(
                       'Sign In',
                       style: TextStyle(
@@ -55,7 +63,12 @@ class _LoginDemoSate extends State<LoginDemo> {
                   ),
                   const SizedBox(width: 20.0),
                   GestureDetector(
-                    onTap: () {},
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) => const SignUp()),
+                      );
+                    },
                     child: const Text(
                       'Sign up',
                       style: TextStyle(
@@ -71,11 +84,7 @@ class _LoginDemoSate extends State<LoginDemo> {
             ),
             const Padding(
               padding: EdgeInsets.only(
-                  left: 15.0,
-                  right: 15.0,
-                  bottom: 20.0,
-                  top: 20.0
-              ),
+                  left: 15.0, right: 15.0, bottom: 20.0, top: 20.0),
               child: TextField(
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
@@ -87,6 +96,7 @@ class _LoginDemoSate extends State<LoginDemo> {
             const Padding(
               padding: EdgeInsets.only(left: 15.0, right: 15.0, bottom: 20),
               child: TextField(
+                obscureText: true,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Password',
@@ -105,14 +115,155 @@ class _LoginDemoSate extends State<LoginDemo> {
                 },
                 child: const Text(
                   'Login',
-                  style: TextStyle(color: Colors.blue, fontSize: 15.0
-                  ),
+                  style: TextStyle(color: Colors.blue, fontSize: 15.0),
                 ),
               ),
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class SignUp extends StatelessWidget {
+  const SignUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Sign up page'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(
+              height: 60.0,
+              width: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) => const LoginDemo()),
+                      );
+                    },
+                    child: const Text(
+                      'Sign In',
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 20.0),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) => const SignUp()),
+                      );
+                    },
+                    child: const Text(
+                      'Sign up',
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 20.0),
+                  Expanded(child: SizedBox()),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(
+                  left: 15.0, right: 15.0, bottom: 20.0, top: 20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Name',
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 15.0, right: 15.0, bottom: 20),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Surname',
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 15.0, right: 15.0, bottom: 20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 15.0, right: 15.0, bottom: 20),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 15.0, right: 15.0, bottom: 20),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Confirm Password',
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 360,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => const Nextsignuppage()),
+                  );
+                },
+                child: const Text(
+                  'Next',
+                  style: TextStyle(color: Colors.blue, fontSize: 15.0),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Nextsignuppage extends StatelessWidget {
+  const Nextsignuppage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      
     );
   }
 }
@@ -128,17 +279,15 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => const MapsPage()),
-              );
-            },
-            child: const Text(
-              'Go to maps'
-          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => const MapsPage()),
+            );
+          },
+          child: const Text('Go to maps'),
         ),
-      ),  
+      ),
     );
   }
 }
