85a2c15f57ae89a518d1c58d6790b4c802cc2216
Bruno Miguel Ataide Fortes <52974677+Bruno081002@users.noreply.github.com>
2024-03-22 17:50:58 +0000

    Adding Vertical Prototype

---
+++ vertical_prototype/Vertical Prototype/lib/main.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Login page',
+      home: LoginDemo(),
+    );
+  }
+}
+
+class LoginDemo extends StatefulWidget {
+  const LoginDemo({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoginDemoSate createState() => _LoginDemoSate();
+}
+
+class _LoginDemoSate extends State<LoginDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Login page'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(height: 60.0, width: 20.0,),
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {},
+                    child: const Text(
+                      'Sign In',
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 20.0),
+                  GestureDetector(
+                    onTap: () {},
+                    child: const Text(
+                      'Sign up',
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 20.0),
+                  Expanded(child: SizedBox()),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(
+                  left: 15.0,
+                  right: 15.0,
+                  bottom: 20.0,
+                  top: 20.0
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                  hintText: 'Enter valid email',
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 15.0, right: 15.0, bottom: 20),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  hintText: 'put you password',
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 360,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => const HomePage()),
+                  );
+                },
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: Colors.blue, fontSize: 15.0
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home page'),
+      ),
+      body: Center(
+        child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const MapsPage()),
+              );
+            },
+            child: const Text(
+              'Go to maps'
+          ),
+        ),
+      ),  
+    );
+  }
+}
+
+class MapsPage extends StatelessWidget {
+  const MapsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Maps'),
+      ),
+    );
+  }
+}
