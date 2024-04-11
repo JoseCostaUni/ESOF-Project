3b6e4b3d98d40ed9b2d799c8ee970eeb77ca527f
Bruno Miguel Ataide Fortes <52974677+Bruno081002@users.noreply.github.com>
2024-04-11 01:37:49 +0100

    working on the app

--- app/app/lib/screens/profile.dart
+++ app/app/lib/screens/profile.dart
@@ -1,6 +1,7 @@
 import 'package:app/screens/createevent.dart';
 import 'package:app/screens/editprofile.dart';
 import 'package:app/screens/homepage.dart';
+import 'package:app/screens/settingpages.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
@@ -31,175 +32,194 @@ class _MyProfilePageState extends State<MyProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(239, 255, 228, 225),
-      body: Column(
+      body: Stack(
         children: [
-          const SizedBox(height: 30),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              GestureDetector(
-                onTap: getImage,
-                child: CircleAvatar(
-                  radius: 60,
-                  backgroundImage: _image != null ? FileImage(_image!) : null,
-                  child: _image == null
-                      ? const Icon(Icons.add_a_photo,
-                          size: 40, color: Colors.white)
-                      : null,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 20),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 25),
-            child: Text(
-              '@guebuzathegoat', // Fazer a ligação à base de dados
-              style: TextStyle(fontSize: 16, color: Colors.grey),
-            ),
-          ),
-          const SizedBox(height: 10),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(right: 145, left: 10),
-                child: Text(
-                  '12', // Fazer a ligação à base de dados
-                  style: TextStyle(fontSize: 16, color: Colors.grey),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(right: 20, left: 30),
-                child: Text(
-                  '2', // Fazer a ligação à base de dados
-                  style: TextStyle(fontSize: 16, color: Colors.grey),
-                ),
-              ),
-            ],
-          ),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(right: 70, left: 10),
-                child: Text(
-                  'Participations', // Fazer a ligação à base de dados
-                  style: TextStyle(fontSize: 16, color: Colors.grey),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(right: 30, left: 30),
-                child: Text(
-                  'Organized', // Fazer a ligação à base de dados
-                  style: TextStyle(fontSize: 16, color: Colors.grey),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 50, left: 10),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context, MaterialPageRoute(builder: (_) => const EditProfile())
-                        );
-                  },
-                  child: const Text('Edit Profile'),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 10, left: 10),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Share Profile'),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 25),
-            child: Text(
-              'This is my alter-ego guebuza the goat for the win aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-              // Fazer a ligação à base de dados
-              style: TextStyle(fontSize: 15, color: Colors.black),
-            ),
-          ),
-          const SizedBox(height: 15),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 25),
-            child: Text(
-              'Last Attended Events:',
-              // Fazer a ligação à base de dados
-              style: TextStyle(
-                  fontSize: 15,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
-          const SizedBox(height: 20),
-          Container(
-            width: 300,
-            height: 100,
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(239, 255, 228, 225),
-              borderRadius: BorderRadius.circular(10.0),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 2,
-                  blurRadius: 5,
-                  offset: const Offset(0, 3),
-                ),
-              ],
-            ),
-            child: Row(
+          SingleChildScrollView(
+            child: Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 40, left: 10),
-                  child: Container(
-                    width: 40,
-                    height: 40,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.blue,
+                const SizedBox(height: 30),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    GestureDetector(
+                      onTap: getImage,
+                      child: CircleAvatar(
+                        radius: 60,
+                        backgroundImage:
+                            _image != null ? FileImage(_image!) : null,
+                        child: _image == null
+                            ? const Icon(Icons.add_a_photo,
+                                size: 40, color: Colors.white)
+                            : null,
+                      ),
                     ),
-                    child: const Padding(
-                      padding: EdgeInsets.only(
-                          right: 8.0), // Add padding only to the right
-                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Text(
+                    '@guebuzathegoat', // Fazer a ligação à base de dados
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
                   ),
                 ),
-                const SizedBox(width: 20),
-                const Flexible(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                const SizedBox(height: 10),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 145, left: 10),
+                      child: Text(
+                        '12', // Fazer a ligação à base de dados
+                        style: TextStyle(fontSize: 16, color: Colors.grey),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 20, left: 30),
+                      child: Text(
+                        '2', // Fazer a ligação à base de dados
+                        style: TextStyle(fontSize: 16, color: Colors.grey),
+                      ),
+                    ),
+                  ],
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 70, left: 10),
+                      child: Text(
+                        'Participations', // Fazer a ligação à base de dados
+                        style: TextStyle(fontSize: 16, color: Colors.grey),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 30, left: 30),
+                      child: Text(
+                        'Organized', // Fazer a ligação à base de dados
+                        style: TextStyle(fontSize: 16, color: Colors.grey),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 50, left: 10),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (_) => const EditProfile()));
+                        },
+                        child: const Text('Edit Profile'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10, left: 10),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Share Profile'),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Text(
+                    'This is my alter-ego guebuza the goat for the win aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+                    // Fazer a ligação à base de dados
+                    style: TextStyle(fontSize: 15, color: Colors.black),
+                  ),
+                ),
+                const SizedBox(height: 15),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Text(
+                    'Last Attended Events:',
+                    // Fazer a ligação à base de dados
+                    style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  width: 300,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(239, 255, 228, 225),
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Row(
                     children: [
-                      SizedBox(height: 10),
-                      Text(
-                        'Palestra sobre música clássica',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 40, left: 10),
+                        child: Container(
+                          width: 40,
+                          height: 40,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.blue,
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.only(
+                                right: 8.0), // Add padding only to the right
+                          ),
                         ),
                       ),
-                      SizedBox(height: 5),
-                      Text(
-                        'Casa da Música de Maputo',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey,
+                      const SizedBox(width: 20),
+                      const Flexible(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 10),
+                            Text(
+                              'Palestra sobre música clássica',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            Text(
+                              'Casa da Música de Maputo',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
                 ),
               ],
+            ),
+          ),
+          Positioned(
+            top: 20,
+            right: 20,
+            child: IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => SettingsPage()));
+              },
             ),
           ),
         ],
@@ -222,7 +242,7 @@ class _MyProfilePageState extends State<MyProfilePage> {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (_) => const HomePage(title: "Home")),
+                          builder: (_) => const HomePage(title: "Home")),
                     );
                   },
                 ),
---
+++ app/app/lib/screens/settingpages.dart
@@ -0,0 +1,172 @@
+import 'package:app/screens/createevent.dart';
+import 'package:app/screens/profile.dart';
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({Key? key});
+
+  @override
+  State<SettingsPage> createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(239, 255, 228, 225),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                const Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search Event',
+                        hintStyle: TextStyle(color: Colors.black),
+                        prefixIcon: Icon(Icons.search, color: Colors.black),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromARGB(255, 202, 178, 172)),
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromARGB(255, 202, 178, 172)),
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        ),
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 213, 177, 168),
+                      ),
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              'Account Center',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          ListTile(
+            title: const Text('Account Center'),
+            subtitle: const Text('Password, security, personal details, ads'),
+            leading: const Icon(Icons.account_circle),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              // Implement your logic here
+            },
+          ),
+          ListTile(
+            title: const Text('Notifications'),
+            subtitle: const Text('Turn off,mute'),
+            leading: const Icon(Icons.notifications),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              // Implement your logic here
+            },
+          ),
+          ListTile(
+            title: const Text('Blocked'),
+            subtitle: const Text('Unblock,block users'),
+            leading: const Icon(Icons.block),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              // Implement your logic here
+            },
+          ),
+          const Divider(color: Colors.black,indent: 20, endIndent: 20,),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: SizedBox(
+              width: 150,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Implement your logic here
+                },
+                child: const Text('Log Out'),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: SizedBox(
+              width: 150,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Implement your logic here
+                },
+                child: const Text('Add Account'),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20), // Definindo o raio de borda
+          child: BottomAppBar(
+            color: const Color.fromARGB(255, 202, 178, 172),
+            shape: const CircularNotchedRectangle(),
+            shadowColor: Colors.black54,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.home),
+                  color: Colors.white,
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(Icons.add_circle_outline),
+                  color: Colors.white,
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => const CreateEvent()));
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.notifications),
+                  color: Colors.white,
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(Icons.person),
+                  color: Colors.white,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => const MyProfilePage(title: "profile")
+                      )
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
